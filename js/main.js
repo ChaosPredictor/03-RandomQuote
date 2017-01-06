b3540860c8ece66f99129a5ba5d07f407378bc6e
Chaos Predictor <kuzi81@gmail.com>
2017-01-06 13:57:12 +0200

    js file added with doc ready

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var quotesArray = [
+	{quote:"Hunger makes a thief of any man.", by:"Pearl S. Buck"},
+	{quote:"We fear the thing we want the most.", by:"Robert Anthony"},
+	{quote:"The earth laughs in flowers.", by:"Ralph Waldo Emerson"},
+	{quote:"1", by:"1"}
+]
+
+$(document).ready(function(){
+	console.log( "ready!" );
+});
+
+
+
